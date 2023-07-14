7c575292b9fbddc07c8355b98f5935730da2cf06
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-07-14 20:15:26 +0100

    Set up routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Book Store</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Books />} />
+          <Route path="categories" element={<Categories />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
