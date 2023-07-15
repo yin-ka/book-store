a9874e3b127c92b2d6642cabe882f765b6bd0910
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-07-15 09:09:41 +0100

    Set up data from array of objects

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,30 +1,33 @@
 import React from 'react';
-import {
-  CircularProgressbar,
-} from 'react-circular-progressbar';
+import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import '../styles/Book.css';
+import PropTypes from 'prop-types';
 
-function Book() {
-  const percentage = 66;
+function Book({
+  genre, title, author, progress, status,
+}) {
   return (
     <div className="book-wrapper">
       <div className="book-info">
-        <p className="book-genre">Action</p>
-        <p className="book-title">The Hunger games</p>
-        <p className="book-author">Sizanne collins</p>
+        <p className="book-genre">{genre}</p>
+        <p className="book-title">{title}</p>
+        <p className="book-author">{author}</p>
         <ul className="book-actions">
-          <li className="book-comment">Comments</li>
-          <li className="book-remove">Remove</li>
-          <li className="book-edit">Edit</li>
+          <li className="book-comment"><span>Comments</span></li>
+          <li className="book-remove"><button type="button"> Remove </button></li>
+          <li className="book-edit"><span>Edit</span></li>
         </ul>
       </div>
       <div className="progress-wrapper">
         <div className="book-progress">
-          <div className="book-progress-bar"><CircularProgressbar value={percentage} /></div>
+          <div className="book-progress-bar"><CircularProgressbar value={progress} /></div>
           <div className="book-progress-data">
-            <div className="book-progress-percentage">64%</div>
-            <div className="book-progress-status">Completed</div>
+            <div className="book-progress-percentage">
+              {progress}
+              %
+            </div>
+            <div className="book-progress-status">{status}</div>
           </div>
         </div>
         <div className="book-current">
@@ -36,5 +39,19 @@ function Book() {
     </div>
   );
 }
+Book.defaultProps = {
+  genre: '',
+  title: '',
+  author: '',
+  progress: '',
+  status: '',
+};
+Book.propTypes = {
+  genre: PropTypes.string,
+  title: PropTypes.string,
+  author: PropTypes.string,
+  progress: PropTypes.string,
+  status: PropTypes.string,
+};
 
 export default Book;
