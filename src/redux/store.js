26ab79dd7db87365cc6ebd2840eb733ce9db0857
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-07-19 17:48:50 +0100

    Connect categoriesReducer to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
 export const store = configureStore({
   reducer: {
     book: bookReducer,
+    category: categoriesReducer,
   },
 });
 
