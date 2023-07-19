2d8cbbd4ee61b4b330fe9b142a36bab2ec4a005c
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-07-19 20:09:20 +0100

    Add dispatch and addBook logic

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
 import '../styles/AddBook.css';
 
 function AddBook() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const booksArr = useSelector((state) => state.book.books);
+  const dispatch = useDispatch();
+  const clickHandler = (e) => {
+    const id = booksArr.length + 1;
+    dispatch(addBook({
+      itemId: `item${id + 1}`,
+      title,
+      author,
+    }));
+    e.preventDefault();
+  };
   return (
     <div className="form-wrapper">
       <h2 className="add-head">ADD NEW BOOK</h2>
       <form className="add-book">
-        <input type="text" name="title" placeholder="Book Title" className="input-one" />
-        <input type="text" name="author" placeholder="Book Author" className="input-two" />
-        <button type="submit" className="add-button">ADD BOOK</button>
+        <input type="text" name="title" placeholder="Book Title" className="input-one" onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" name="author" placeholder="Book Author" className="input-two" onChange={(e) => setAuthor(e.target.value)} />
+        <button type="submit" className="add-button" onClick={(e) => { clickHandler(e); }}>ADD BOOK</button>
       </form>
     </div>
   );
