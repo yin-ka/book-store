4d8fd18aa38e8927763babfcfc2f26101c3b3a5b
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-07-20 17:09:46 +0100

    Add the addbook and remove book slices

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -15,6 +15,21 @@ export const fetchBooks = createAsyncThunk(
   },
 );
 
+export const addBook = createAsyncThunk(
+  'books/addBook',
+  async (book) => {
+    const response = await axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/tmfWTBasohouxTHLU5Dm/books', book);
+    return response.data;
+  },
+);
+
+export const removeBook = createAsyncThunk(
+  'books/removeBook',
+  async (bookId) => {
+    await axios.delete(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/tmfWTBasohouxTHLU5Dm/books/${bookId}`);
+    return bookId;
+  },
+);
 export const BookSlice = createSlice({
   name: 'books',
   initialState,
@@ -38,10 +53,16 @@ export const BookSlice = createSlice({
       .addCase(fetchBooks.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message;
+      })
+      .addCase(addBook.fulfilled, (state, action) => {
+        state.books.push(action.payload);
+      })
+      .addCase(removeBook.fulfilled, (state, action) => {
+        state.books = state.books.filter((book) => book.id !== action.payload);
       });
   },
 });
 
-export const { addBook, removeBook } = BookSlice.actions;
+// export const { addBook, removeBook } = BookSlice.actions;
 
 export default BookSlice.reducer;
