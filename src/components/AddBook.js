68e7520120da2b975178c9cc67a2c9fe6526280d
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-07-21 02:35:31 +0100

    Refine Addbook.js

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -11,21 +11,24 @@ function AddBook() {
 
   const clickHandler = (e) => {
     e.preventDefault();
-    const id = Math.floor(Math.random() * 1000000);
-    dispatch(addBook({
-      item_id: `itemId${id + 1}`,
-      title,
-      author,
-      category: 'Action',
-    }));
+    if (title !== '' && author !== '') {
+      const id = Math.floor(Math.random() * 1000000);
+      dispatch(addBook({
+        item_id: `itemId${id + 1}`,
+        title,
+        author,
+        category: 'Action',
+      }));
+      setTitle('');
+      setAuthor('');
+    }
   };
-
   return (
     <div className="form-wrapper">
       <h2 className="add-head">ADD NEW BOOK</h2>
       <form className="add-book">
-        <input type="text" name="title" placeholder="Book Title" className="input-one" onChange={(e) => setTitle(e.target.value)} />
-        <input type="text" name="author" placeholder="Book Author" className="input-two" onChange={(e) => setAuthor(e.target.value)} />
+        <input type="text" name="title" placeholder="Book Title" className="input-one" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" name="author" placeholder="Book Author" className="input-two" value={author} onChange={(e) => setAuthor(e.target.value)} />
         <Button classname="add-button" onClick={clickHandler} title="ADD BOOK" />
       </form>
     </div>
